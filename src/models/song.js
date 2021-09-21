0fc0ee83f7602ca690c467397ed780530a69c792
Daniel Vinet <daniel1vinet@Gmail.com>
2021-09-21 09:43:07 -0300

    Creator require

--- src/models/song.js
+++ src/models/song.js
@@ -3,8 +3,8 @@ const { Schema, model } = require('mongoose');
 const SongSchema = new Schema({
     title: { type: String, required: true },
     lyric: { type: String, required: true },
+    creator: { type: String, required: true },
     author: { type: String, required: false },
-    creator: { type: String, required: false },
     labels: { type: Array, required: false },
     rating: { type: Array, required: false }
 });
